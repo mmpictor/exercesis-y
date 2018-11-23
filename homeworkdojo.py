3e813658251502ea6571880853d68cd7755ef4d9
Micaela <mpictorgarcia@gmail.com>
2018-11-23 11:26:55 -0500

    Create homeworkdojo.py
    convert km, miles
    practice function def

---
+++ homeworkdojo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 23 10:29:44 2018
+
+@author: MASTER
+"""
+km = 0
+miles = 2
+def convert (km, miles): 
+    km = 1.6 * miles 
+    print('km is= ')
+    print (km)
+    
+resul = convert (km, miles)
+
+#def convert(miles):
+#    return 1.6 * miles 
+#print(convert(3))
